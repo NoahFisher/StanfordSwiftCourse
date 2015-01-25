e5223b1f641b140c9d190222494c69f1faecd0ed
Noah <noah.fisher@gmail.com>
2015-01-25 17:40:07 -0500

    week 1: Logistics, IOS 8 Overview

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Noah Fisher on 1/25/15.
+//  Copyright (c) 2015 Noah Fisher. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController
+{
+    @IBOutlet weak var display: UILabel!
+    
+    var userIsInTheMiddleOfTypingANumber: Bool = false
+    
+    @IBAction func appendDigit(sender: UIButton) {
+        let digit = sender.currentTitle!
+        if userIsInTheMiddleOfTypingANumber {
+            display.text = display.text! + digit
+        } else {
+            userIsInTheMiddleOfTypingANumber = true
+            display.text = digit
+        }
+    }
+}
+
